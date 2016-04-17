0ce5724d3d6cb1b9e950ff33fd9eace8d8e92a3b
Andrew Curtin <adcurtin@gmail.com>
2016-04-17 01:44:30 -0400

    debug version with working steering wheel -> bluetooth

--- wheel_decoder.ino
+++ wheel_decoder.ino
@@ -10,13 +10,10 @@
 
 HardwareSerial & kbus = Serial1;
 
-int kbus_from = 0;
-int kbus_length = 0;
-int kbus_to = 0;
-int kbus_data[32] = { 0 };
+byte kbus_data[32] = { 0 };
 
-// String message = "adcurtin";
-String message = "123456789abcdef";
+String message = "adcurtin";
+// String message = "123456789ab"; //max length message
 
 
 void setup(){
@@ -27,6 +24,8 @@ void setup(){
     digitalWrite(PLAY_PIN, LOW);
     digitalWrite(NEXT_PIN, LOW);
     digitalWrite(PREV_PIN, LOW);
+
+    pinMode(0, INPUT_PULLUP);
 
 #ifdef DEBUG
     Serial.begin(57600);
@@ -42,15 +41,13 @@ void setup(){
     digitalWrite(EN_PIN, HIGH);
 
     kbus.begin(9600, SERIAL_8E1);
+    kbus_print(message);
 
 }
 
 void loop(){
 
-    // kbus_print2(message);
-    // delay(1000);
-    // kbus_print(message);
-    delay(5000);
+    delay(10);
 
     // press(PLAY_PIN);
     // Serial.println("pressed play");
@@ -65,72 +62,79 @@ void loop(){
 
 
     int bytes_read = 0;
-    // bytes_read = read_kbus_packet();
+    bytes_read = read_kbus_packet();
     if(bytes_read > 0){
-        // if(kbus_data[0] == 0x68) print_packet();
-        // parse_packet();
+        // if(kbus_data[2] == 0x68 || kbus_data[2] == 0xC8) {
+        //     Serial.println("recd packet to radio or phone");
+        //     print_packet();
+        // }
+        parse_packet();
     }
-    delay(2000);
+    // delay(200);
 
 
 }
 
 /* check kbus for data. if any available, block until whole packet is read.
- * returns number of bytes read.
+ * returns:
+ *      int - number of bytes read.
  * updates:
- *      kbus_from       ID of sending device
- *      kbus_length     length of rest of message
- *      kbus_data[]     rest of message (first byte destination ID)
+ *      kbus_data[]     contains kbus packet
  */
 int read_kbus_packet(){
     if(kbus.available() > 1){
-        kbus_from = kbus.read();   //first byte of message is ID of sending device
-        kbus_length = kbus.read(); //second byte is bytes remaining in packet
+        kbus_data[0] = kbus.read(); //first byte of message is ID of sending device
+        kbus_data[1] = kbus.read(); //second byte is bytes remaining in packet
+        // Serial.println("read 2 bytes from kbus packet");
     } else {
         return 0; //nothing available, nothing read.
     }
 
-    for(int i = 0; i < kbus_length; i++){
+    for(int i = 0; i < kbus_data[1]; i++){
         while(kbus.available() == 0){} //wait for data to be available
-        kbus_data[i] = kbus.read();
+        kbus_data[2 + i] = kbus.read();
     }
-    kbus_to = kbus_data[0];
 
     //verify checksum
-    byte crc = kbus_from ^ kbus_length;
-    for(int i = 0; i < (kbus_length - 1); i++){
-        crc ^= kbus_data[i];
-    }
+    byte crc = checksum(kbus_data, 2 + kbus_data[1]);
 
-    if(kbus_data[kbus_length-1] != crc){
+    if(kbus_data[1 + kbus_data[1]] != crc){
 #ifdef DEBUG
+        Serial.println("-------------------------");
         Serial.print("Checksum mismatch! expected: ");
         Serial.print(crc, HEX);
         Serial.print(" got: ");
-        Serial.println(kbus_data[kbus_length-1], HEX);
+        Serial.println(kbus_data[1 + kbus_data[1]], HEX);
         print_packet();
+        Serial.println("-------------------------x");
 #endif
         return 0; //no valid bytes read. can't really handle errors.
     }
 
-    return kbus_length + 2;
+    return 2 + kbus_data[1];
 }
 
 void parse_packet(){
-    if(kbus_data[0] == 0x68){ //command sent to radio
-        if(kbus_data[1] == 0x32) { //volume button
-            if(kbus_data[2] == 0x11){ //volume up
+    if(kbus_data[2] == 0x68){ //command sent to radio
+        if(kbus_data[3] == 0x32) { //volume button
+            if(kbus_data[4] == 0x11){ //volume up
 
-            } else if(kbus_data[2] == 0x10){ //volume down
+            } else if(kbus_data[4] == 0x10){ //volume down
 
             }
-        } else if (kbus_data[1] == 0x3B){ //track button
-            switch (kbus_data[2]) {
+        } else if (kbus_data[3] == 0x3B){ //track button
+            switch (kbus_data[4]) {
                 case 0x01: //next pressed
+                    press(NEXT_PIN);
+                    Serial.println("next pressed");
+                    break;
                 case 0x11: //next held
                 case 0x21: //next released
                     break;
                 case 0x08: //previous pressed
+                    press(PREV_PIN);
+                    Serial.println("previous pressed");
+                    break;
                 case 0x18: //previous held
                 case 0x28: //previous released
                     break;
@@ -138,12 +142,20 @@ void parse_packet(){
                     break;
             }
         }
-    } else if (kbus_data[0] == 0xC8){ //command send to phone unit
-        if(kbus_data[1] == 0x01){ // R/T button pressed
-        } else if(kbus_data[1] == 0x3B){ // phone button
-            if(kbus_data[2] == 0x27){ // pressed
-            } else if(kbus_data[2] == 0x37){ //held
-            } else if(kbus_data[2] == 0x07){ //released
+    } else if (kbus_data[2] == 0xC8){ //command send to phone unit
+        if(kbus_data[3] == 0x01){ // R/T button pressed
+        } else if(kbus_data[3] == 0x3B){ // phone button
+            if(kbus_data[4] == 0x80){ // pressed
+                //need to handle holding of this.
+
+            } else if(kbus_data[4] == 0x90){ //held
+                //if held, this is hold the play button, the press function will
+                //release it when the button is released.
+                digitalWrite(PLAY_PIN, HIGH);
+                Serial.println("play held");
+            } else if(kbus_data[4] == 0xA0){ //released
+                press(PLAY_PIN);
+                Serial.println("play pressed");
             }
         }
     } else {
@@ -171,9 +183,11 @@ void parse_packet(){
                         // hold                3B:90:37:
                         // release                3B:A0:07:
 
-byte checksum(byte data[], int length){
+//packet length includes the checksum
+byte checksum(byte data[], int packet_length){
     byte crc = 0;
-    for(int i=0;i<length;i++){
+    //the last byte of the packet is the checksum, don't include the previous checksum in the calculation
+    for(int i=0; i < (packet_length - 1); i++){
         crc ^= data[i];
     }
     return crc;
@@ -181,28 +195,22 @@ byte checksum(byte data[], int length){
 
 #ifdef DEBUG
 void print_packet(){
-    Serial.println("kbus packet: ");
-    Serial.print("sender ID: ");
-    Serial.println(kbus_from, HEX);
-    Serial.print("message length: ");
-    Serial.println(kbus_length, HEX);
-    Serial.print("receiver ID: ");
-    Serial.println(kbus_to, HEX);
-    Serial.println("message: ");
-    for(int i = 1; i < kbus_length; i++){
-        Serial.println(kbus_data[i], HEX);
+    Serial.print("recieved kbus packet: ");
+    for(int i = 0; i < (2+kbus_data[1]); i++){
+        Serial.print(kbus_data[i], HEX);
+        Serial.print(".");
     }
+    Serial.println("");
 }
 
 void print_part(int start){
-    for(int i = start; i < kbus_length; i++){
+    for(int i = start; i < (2+kbus_data[1]); i++){
         Serial.print(kbus_data[i], HEX);
         Serial.print(":");
     }
     Serial.println("");
 }
 #endif
-
 
 //takes a string and prints the first 11 characters to the display
 //need to fix scrolling later
@@ -227,7 +235,7 @@ void kbus_print(String message){
 
     int out_len = 2 + out_data[1]; //will always transmit right number of bytes
     kbus.write(out_data, out_len);
-    delay(200); //low priority messages, idle for 200ms after transmission
+    delay(2); //low priority messages, idle for 2ms after transmission
     Serial.print("kbus send data: ");
     for(i=0;i<out_len;i++){
         Serial.print(out_data[i], HEX);
@@ -236,8 +244,6 @@ void kbus_print(String message){
     Serial.println("");
     return;
 }
-
-
 
 //press a button for 200ms
 void press(int pin){
