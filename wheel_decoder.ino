31d08c6df9b5c85d95a589c98b45d871032b777c
Andrew Curtin <adcurtin@gmail.com>
2016-05-08 01:56:38 -0400

    added timeout to reading the kbus too, in we get out of sync there

--- wheel_decoder.ino
+++ wheel_decoder.ino
@@ -44,7 +44,9 @@ IntervalTimer holdoffTimer, metaTimer;
 // String message = "123456789ab"; //max length message
 
 String title = "";
+String oldTitle = "";
 String artist = "";
+String oldArtist = "";
 String album = "";
 
 //read buffer
@@ -56,7 +58,7 @@ volatile int buf_i = 0;
 volatile int buf_len = 0;
 volatile int int_count = 0;
 
-elapsedMillis tref = 0, dot;//, bc127_timeout = 0;
+elapsedMillis tref = 0;//, kbus_timeout;//, bc127_timeout = 0;
 
 void setup(){
 
@@ -65,7 +67,7 @@ void setup(){
 
     bc127.begin(57600);
 
-#ifdef DEBUG
+    #ifdef DEBUG
     usb.begin(57600);
     pinMode(LED_PIN, OUTPUT);
     digitalWrite(LED_PIN, HIGH);
@@ -76,7 +78,7 @@ void setup(){
     usb.write(27); // ESC
     usb.print("[H"); // cursor to home
     usb.print("started\r");
-#endif
+    #endif
 
     // bc127.attachRts(11);
     bc127.clear();
@@ -104,7 +106,6 @@ void setup(){
     // UCSR1C = 0x26; // manually set register to SERIAL_8E1 for teensy 2 (leonardo too)
     // kbus.begin(9600);
     kbus_print("adcurtin");
-    // update_display_buffer("adcurtin", 1);
 
 }
 
@@ -115,30 +116,17 @@ void loop(){
     int bytes_read = 0;
     bytes_read = read_kbus_packet();
     if(bytes_read > 0){
+        #ifdef DEBUG
         // if(kbus_data[2] == 0x68 || kbus_data[2] == 0xC8) {
             // usb.println("recd packet to radio or phone");
             // print_packet();
         // }
+        #endif
         parse_packet();
     }
 
-
     //read bc127
     read_and_parse_bc127_packet();
-    // if(dot > 250)
-    // {
-    //     usb.print('.');
-    //     dot = 0;
-    // }
-
-    //print new line of display every 3 seconds
-    // if (pref + 3000 < millis())
-    // {
-    //     print_buffer();
-    // }
-
-
-
 }
 
 /* check kbus for data. if any available, block until whole packet is read.
@@ -148,24 +136,36 @@ void loop(){
  *      kbus_data[]     contains kbus packet
  */
 int read_kbus_packet(){
+    elapsedMillis kbus_timeout = 0;
     if(kbus.available() > 1){
+        // kbus_timeout = 0;
         kbus_data[0] = kbus.read(); //first byte of message is ID of sending device
         kbus_data[1] = kbus.read(); //second byte is bytes remaining in packet
-        // usb.println("read 2 bytes from kbus packet");
     } else {
         return 0; //nothing available, nothing read.
     }
 
     for(int i = 0; i < kbus_data[1]; i++){
-        while(kbus.available() == 0){} //wait for data to be available
-        kbus_data[2 + i] = kbus.read();
+        while(kbus_timeout < 2000){
+            if(kbus.available()){
+                kbus_data[2 + i] = kbus.read();
+                break; //out of while loop, read next data
+            }
+        } //wait for data to be available
+    }
+    if(kbus_timeout >= 2000){
+        #ifdef DEBUG
+        usb.print("kbus timeout!\r");
+        #endif
+        kbus.clear(); //we might've gotten out of sync. throw everything away, hopefully not in the middle of a packet
     }
 
     //verify checksum
     byte crc = checksum(kbus_data, 2 + kbus_data[1]);
 
     if(kbus_data[1 + kbus_data[1]] != crc){
-#ifdef DEBUG
+        kbus.clear(); //we might've gotten out of sync. throw everything away, hopefully not in the middle of a packet
+        #ifdef DEBUG
         usb.print("-------------------------\r");
         usb.print("Checksum mismatch! expected: ");
         usb.print(crc, HEX);
@@ -173,7 +173,7 @@ int read_kbus_packet(){
         usb.println(kbus_data[1 + kbus_data[1]], HEX);
         print_packet();
         usb.print("-------------------------x\r");
-#endif
+        #endif
         return 0; //no valid bytes read. can't really handle errors.
     }
 
@@ -192,17 +192,15 @@ void parse_packet(){
         } else if (kbus_data[3] == 0x3B){ //track button
             switch (kbus_data[4]) {
                 case 0x01: //next pressed
-                    // press(NEXT_PIN);
-                    // usb.println("next pressed");
                     bc127_command("MUSIC 11 FORWARD");
+                    metaTimer.begin(metadata_request, 5000000); // query the metadata in 5 seconds
                     break;
                 case 0x11: //next held
                 case 0x21: //next released
                     break;
                 case 0x08: //previous pressed
-                    // press(PREV_PIN);
-                    // usb.println("previous pressed");
                     bc127_command("MUSIC 11 BACKWARD");
+                    metaTimer.begin(metadata_request, 5000000); // query the metadata in 5 seconds
                     break;
                 case 0x18: //previous held
                 case 0x28: //previous released
@@ -231,15 +229,9 @@ void parse_packet(){
                 else {
                     bc127_command("MUSIC 11 PLAY");
                 }
-                //the normal read will catch the source's response of AVRCP_PLAY \ AVRCP_PAUSE and update music_playing
+                //the normal read will catch the source's response of AVRCP_PLAY/AVRCP_PAUSE and update music_playing
             } else if(kbus_data[4] == 0x90){ //held
-                //if held, this is hold the play button, the press function will
-                //release it when the button is released.
-                // digitalWrite(PLAY_PIN, HIGH);
-                // usb.println("play held");
             } else if(kbus_data[4] == 0xA0){ //released
-                // press(PLAY_PIN);
-                // usb.println("play pressed");
             }
         }
     } else {
@@ -326,18 +318,26 @@ void kbus_print(String message){
         usb.print("waiting for kbus holdoff\r");
         #endif
     }
-    if(clearToSend) kbus.write(out_data, out_len);
+    if (clearToSend)
+    {
+        // noInterrupts();
+        kbus.clear(); //there shouldn't be anything on new on the bus if we're clear to send
+        kbus.write(out_data, out_len);
+        //we should get this data right back
+    }
     // delay(2); //low priority messages, idle for 2ms after transmission
+    #ifdef DEBUG
     // usb.print("kbus send data: ");
     // for(i=0;i<out_len;i++){
     //     usb.print(out_data[i], HEX);
     //     usb.print(":");
     // }
-    // // usb.println("");
+    // usb.print("\r");
 
     usb.print("kbus_print: ");
     usb.print(message.c_str());
     usb.print("\r");
+    #endif
     return;
 }
 
@@ -376,14 +376,16 @@ void print_buffer()
 void update_display_buffer(String message, byte reset)
 {
     int i = 0;
-    if (reset == 1)
+    if (reset == 1 || buf_len > DISPLAY_BUFFER_SIZE)
     {
+        // noInterrupts();
         buf_len = 0;
         buf_i = 0;
         for (i = 0; i < 8; i++)
         {
             display_buffer[i] = "";
         }
+        // interrupts();
     }
 
     if (message.length() > 55) message = message.substring(0,55); //limit each data to display cycles so whole message can be displayed
@@ -393,10 +395,11 @@ void update_display_buffer(String message, byte reset)
     {
         //copy first 11 characters of message to display buffer (+ null byte?)
         // message.toCharArray(display_buffer[buf_len], 12);
+        // noInterrupts();
         display_buffer[buf_len] = message.substring(0, 11);
-
         buf_len++;
-        if (buf_len > DISPLAY_BUFFER_SIZE) buf_len = 0; //don't handle gracefully
+        // if (buf_len > DISPLAY_BUFFER_SIZE) buf_len = 0; //don't handle gracefully
+        // interrupts();
         //remove first 11 chars from message (0 indexed)
         message = message.substring(11, message.length());
     }
@@ -408,7 +411,6 @@ void update_display_buffer(String message, byte reset)
 
     // //bounds check on buffer
     // if (buf_len >= 8) buf_len = 0;
-
     return;
 }
 
@@ -417,9 +419,11 @@ void update_display_buffer(String message, byte reset)
  */
 byte bc127_command(const char message[])
 {
+    #ifdef DEBUG
     usb.print("bc127 command: ");
     usb.print(message);
     usb.print("\r");
+    #endif
     bc127.print(message);
     bc127.print("\r");
 
@@ -435,22 +439,28 @@ byte bc127_command(const char message[])
         }
         if (bc127_timeout > 2000) //timeout after 200ms
         {
+            #ifdef DEBUG
             usb.print("timeout reading bc127 command response!\r");
             usb.print("read: \'");
             usb.print(bc127_buffer.c_str());
             usb.print("\r");
+            #endif
             bc127_buffer = "";
             bc127_timeout = 0;
         }
     }
     //finished reading a line: bc127_buffer ends with \r
 
+    #ifdef DEBUG
     usb.print("bc127 rx response: ");
     usb.print(bc127_buffer);
+    #endif
     //interpret data
     if (bc127_buffer.startsWith("ER"))
     {
+            #ifdef DEBUG
             usb.print("bc127 command error\r");
+            #endif
             return 0;
     }
     if (bc127_buffer.startsWith("OK")) return 1;
@@ -463,9 +473,6 @@ void read_and_parse_bc127_packet()
     if(!bc127.available()) return;
     bc127_buffer = "";
     elapsedMillis bc127_timeout = 0;
-    // usb.print("bc127_timeout:");
-    // usb.print(bc127_timeout);
-    // usb.print("\r");
     while(!bc127_buffer.endsWith("\r"))
     {
         if (bc127.available())
@@ -480,12 +487,14 @@ void read_and_parse_bc127_packet()
         }
         if (bc127_timeout > 3000) //timeout after 200ms
         {
+            #ifdef DEBUG
             usb.print("read: \'");
             usb.print(bc127_buffer.c_str());
             usb.print("\'\r");
             usb.print("timeout reading bc127 packet: ");
             usb.print(bc127_timeout);
             usb.print("\r");
+            #endif
             bc127_buffer = "";
             bc127_timeout = 0;
             return;
@@ -493,44 +502,54 @@ void read_and_parse_bc127_packet()
     }
     //finished reading a line: bc127_buffer ends with \r
 
+    #ifdef DEBUG
     usb.print("bc127 rx: ");
     usb.print(bc127_buffer);
+    #endif
     //interpret data
     if (bc127_buffer.startsWith("AVRCP_PLAY 11") || bc127_buffer.startsWith("A2DP_STREAM_START 10"))
     {
         if(!music_playing)
         {
-            metaTimer.begin(metadata_request, 5000000);
+            metaTimer.begin(metadata_request, 5000000);// query the metadata in 5 seconds.
         }
         music_playing = 1;
+        #ifdef DEBUG
         usb.print("music playing\r");
+        #endif
     }
     else if (bc127_buffer.startsWith("AVRCP_PAUSE 11") || bc127_buffer.startsWith("A2DP_STREAM_SUSPEND 10"))
     {
         music_playing = 0;
+        #ifdef DEBUG
         usb.print("music paused\r");
+        #endif
     }
     else if (bc127_buffer.startsWith("AVRCP_MEDIA TITLE: "))
     {
         //0xc6 is a speaker in the e46 business cd
         title = (char) 0xc6 + bc127_buffer.substring(19, bc127_buffer.length() - 1);  // length() - 1 to trim the /r
-        update_display_buffer(title, 1);
-        // usb.print("parsed title\r");
     }
     else if (bc127_buffer.startsWith("AVRCP_MEDIA ARTIST: "))
     {
         //0xc4 is a music note in the e46 business cd
         artist = (char) 0xc4 + bc127_buffer.substring(20, bc127_buffer.length() - 1);  // length() - 1 to trim the /r
-        update_display_buffer(artist, 0);
-        // usb.print("parsed artist\r");
     }
     else if (bc127_buffer.startsWith("AVRCP_MEDIA ALBUM: "))
     {
         album = bc127_buffer.substring(19, bc127_buffer.length() - 1); // length() - 1 to trim the /r
         // update_display_buffer(album, 0);
-        // usb.print("parsed album\r");
-        // bc127.clear(); //clear the rest of the buffer, usually "AVRCP_MEDIA PLAYING_TIME(MS):"" which often times out
 
+        if ( title.compareTo(oldTitle) != 0 || artist.compareTo(oldArtist) != 0)
+        {
+            update_display_buffer(title, 1);
+            update_display_buffer(artist, 0);
+            oldTitle = title;
+            oldArtist = artist;
+            #ifdef DEBUG
+            // usb.print("metadata changed. updated display and copied new to old\r");
+            #endif
+        }
         //we've finished reading in the metadata we care about, print the buffer
         //could move this to "AVRCP_MEDIA PLAYING_TIME(MS): " clause
         // print_buffer();
@@ -571,6 +590,8 @@ void endHoldoff()
 void metadata_request()
 {
     metaTimer.end(); //single shot
+    #ifdef DEBUG
     usb.print("meta request timer\r");
+    #endif
     bc127_command("AVRCP_META_DATA 11");
 }
